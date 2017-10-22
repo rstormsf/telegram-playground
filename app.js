0c691adc5581ec493fe9f5f881e17daa175f6072
Roman Storm <rstormsf@gmail.com>
2017-10-22 10:31:25 -0700

    sdfd

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 const { NODE_ENV, PORT, SCROBBLERBOT_TOKEN } = require('./config');
-const { sendToAdmin } = require('./src/helpers/utils');
+// const { sendToAdmin } = require('./src/helpers/utils');
 const bot = require('./src/bot');
 
 
 if (NODE_ENV === 'production') {
   bot.startWebhook(`/${SCROBBLERBOT_TOKEN}`, null, PORT);
 } else if (NODE_ENV === 'development') {
-  const tlsOptions = {
-    key: fs.readFileSync('key.key'),
-    cert: fs.readFileSync('cert.pem'),
-  };
+  // const tlsOptions = {
+  //   key: fs.readFileSync('server.pem'),
+  //   cert: fs.readFileSync('server.pem'),
+  // };
 
-  bot.startWebhook(`/${SCROBBLERBOT_TOKEN}`, tlsOptions, PORT);
+  bot.startWebhook(`/${SCROBBLERBOT_TOKEN}`, null, PORT);
 }
 
 console.log(`Server is listening to localhost:${PORT}`);
 
-process.on('unhandledRejection', (e) => {
-  console.log(e);
-  sendToAdmin(`Unhandled Rejection! ${e.message}`);
-});
+// process.on('unhandledRejection', (e) => {
+//   console.log(e);
+//   sendToAdmin(`Unhandled Rejection! ${e.message}`);
+// });
 
-process.on('uncaughtException', (e) => {
-  console.log(e);
-  sendToAdmin(`Uncaught Exception! ${e.message}`);
-});
+// process.on('uncaughtException', (e) => {
+//   console.log(e);
+//   sendToAdmin(`Uncaught Exception! ${e.message}`);
+// });
--- src/bot.js
+++ src/bot.js
@@ -1,27 +1,27 @@
 const Telegraf = require('telegraf');
-const { scrobbleTracks, scrobbleTrackFromText } = require('./helpers/scrobbler');
-const { searchFromLastfmAndAnswerInlineQuery } = require('./helpers/actions');
-const user = require('./middlewares/user');
-const scenes = require('./middlewares/scenes');
-const auth = require('./middlewares/auth');
-const session = require('./middlewares/session');
-const limiter = require('./middlewares/limiter');
-const logger = require('./middlewares/logger');
-const error = require('./middlewares/error');
+// const { scrobbleTracks, scrobbleTrackFromText } = require('./helpers/scrobbler');
+// const { searchFromLastfmAndAnswerInlineQuery } = require('./helpers/actions');
+// const user = require('./middlewares/user');
+// const scenes = require('./middlewares/scenes');
+// const auth = require('./middlewares/auth');
+// const session = require('./middlewares/session');
+// const limiter = require('./middlewares/limiter');
+// const logger = require('./middlewares/logger');
+// const error = require('./middlewares/error');
 const { SCROBBLERBOT_TOKEN } = require('../config');
-const { successfulScrobble, requestError, multipleArray, sendToAdmin } = require('./helpers/utils');
-const { findSucceededMessageById, findFailedMessageById } = require('./helpers/dbmanager');
-require('./helpers/scheduler');
-require('./db');
+// const { successfulScrobble, requestError, multipleArray, sendToAdmin } = require('./helpers/utils');
+// const { findSucceededMessageById, findFailedMessageById } = require('./helpers/dbmanager');
+// require('./helpers/scheduler');
+// require('./db');
 
 
 const bot = new Telegraf(SCROBBLERBOT_TOKEN, {
   telegram: {
-    webhookReply: false,
+    webhookReply: true
   },
 });
 
-bot.context.user = null;
+// bot.context.user = null;
 
 setImmediate(async () => {
   try {
@@ -32,103 +32,112 @@ setImmediate(async () => {
   }
 });
 
-bot.use(error);
-bot.use(user);
-bot.use(session);
-bot.use(logger);
-bot.use(scenes);
+// bot.use(error);
+// bot.use(user);
+// bot.use(session);
+// bot.use(logger);
+// bot.use(scenes);
 
-bot.hears(/\/\w+/, async (ctx) => {
-  await ctx.reply('If you are confused type /help');
+// bot.hears(/\/\w+/, async (ctx) => {
+//   await ctx.reply('If you are confused type /help');
+// });
+
+bot.on('text', (ctx) => {
+  console.log(ctx.from.username);
 });
 
-bot.on('text', auth, limiter, async (ctx) => {
-  await scrobbleTrackFromText(ctx);
-});
+bot.command('start', async ({ session, reply, from }) => {
+  console.log('asdf');
+  await reply('Hi there, please choose an option');
+})
 
-bot.on('inline_query', async (ctx) => {
-  await searchFromLastfmAndAnswerInlineQuery(ctx, 'track');
-});
+bot.action('mda', ({ session, reply, from }) => {
+  console.log('asdf');
+  return reply('Hi there, please choose an option');
+})
+// bot.on('inline_query', async (ctx) => {
+//   await searchFromLastfmAndAnswerInlineQuery(ctx, 'track');
+// });
 
-bot.action('CANCEL', async (ctx) => {
-  await ctx.flow.leave();
-  await ctx.editMessageText('Canceled');
-});
+// bot.action('CANCEL', async (ctx) => {
+//   await ctx.flow.leave();
+//   await ctx.editMessageText('Canceled');
+// });
 
-bot.action('RETRY', limiter, async (ctx) => {
-  ctx.flow.state.messageIdToEdit = (await ctx.editMessageText('<i>Scrobbling...</i>',
-    Telegraf.Extra.HTML())).message_id;
-  const messageId = ctx.callbackQuery.message.message_id;
-  const message = await findFailedMessageById(messageId);
+// bot.action('RETRY', limiter, async (ctx) => {
+//   ctx.flow.state.messageIdToEdit = (await ctx.editMessageText('<i>Scrobbling...</i>',
+//     Telegraf.Extra.HTML())).message_id;
+//   const messageId = ctx.callbackQuery.message.message_id;
+//   const message = await findFailedMessageById(messageId);
 
-  if (!message) {
-    await ctx.editMessageText('Expired');
-    return;
-  }
+//   if (!message) {
+//     await ctx.editMessageText('Expired');
+//     return;
+//   }
 
-  try {
-    await scrobbleTracks(message.tracks, undefined, ctx.user.key);
-  } catch (e) {
-    await requestError(ctx, e);
-    return;
-  }
+//   try {
+//     await scrobbleTracks(message.tracks, undefined, ctx.user.key);
+//   } catch (e) {
+//     await requestError(ctx, e);
+//     return;
+//   }
 
-  await successfulScrobble(ctx);
-});
+//   await successfulScrobble(ctx);
+// });
 
-bot.action('REPEAT', async (ctx) => {
-  const messageId = ctx.callbackQuery.message.message_id;
-  const message = await findSucceededMessageById(messageId);
+// bot.action('REPEAT', async (ctx) => {
+//   const messageId = ctx.callbackQuery.message.message_id;
+//   const message = await findSucceededMessageById(messageId);
 
-  if (!message) {
-    await ctx.editMessageText('Expired');
-    return;
-  }
+//   if (!message) {
+//     await ctx.editMessageText('Expired');
+//     return;
+//   }
 
-  await ctx.editMessageText('How many times do you want to scrobble this again?',
-    Telegraf.Markup.inlineKeyboard([
-      [
-        Telegraf.Markup.callbackButton('1', 'REPEAT:1'),
-        Telegraf.Markup.callbackButton('2', 'REPEAT:2'),
-        Telegraf.Markup.callbackButton('3', 'REPEAT:3'),
-        Telegraf.Markup.callbackButton('4', 'REPEAT:4'),
-        Telegraf.Markup.callbackButton('5', 'REPEAT:5'),
-      ],
-      [
-        Telegraf.Markup.callbackButton('6', 'REPEAT:6'),
-        Telegraf.Markup.callbackButton('7', 'REPEAT:7'),
-        Telegraf.Markup.callbackButton('8', 'REPEAT:8'),
-        Telegraf.Markup.callbackButton('9', 'REPEAT:9'),
-        Telegraf.Markup.callbackButton('10', 'REPEAT:10'),
-      ],
-    ]).extra());
-});
+//   await ctx.editMessageText('How many times do you want to scrobble this again?',
+//     Telegraf.Markup.inlineKeyboard([
+//       [
+//         Telegraf.Markup.callbackButton('1', 'REPEAT:1'),
+//         Telegraf.Markup.callbackButton('2', 'REPEAT:2'),
+//         Telegraf.Markup.callbackButton('3', 'REPEAT:3'),
+//         Telegraf.Markup.callbackButton('4', 'REPEAT:4'),
+//         Telegraf.Markup.callbackButton('5', 'REPEAT:5'),
+//       ],
+//       [
+//         Telegraf.Markup.callbackButton('6', 'REPEAT:6'),
+//         Telegraf.Markup.callbackButton('7', 'REPEAT:7'),
+//         Telegraf.Markup.callbackButton('8', 'REPEAT:8'),
+//         Telegraf.Markup.callbackButton('9', 'REPEAT:9'),
+//         Telegraf.Markup.callbackButton('10', 'REPEAT:10'),
+//       ],
+//     ]).extra());
+// });
 
-bot.action(/REPEAT:\d?\d/, limiter, async (ctx) => {
-  ctx.flow.state.messageIdToEdit = (await ctx.editMessageText('<i>Scrobbling...</i>',
-    Telegraf.Extra.HTML())).message_id;
-  const messageId = ctx.callbackQuery.message.message_id;
-  const message = await findSucceededMessageById(messageId);
-  const count = ctx.callbackQuery.data.split(':')[1];
+// bot.action(/REPEAT:\d?\d/, limiter, async (ctx) => {
+//   ctx.flow.state.messageIdToEdit = (await ctx.editMessageText('<i>Scrobbling...</i>',
+//     Telegraf.Extra.HTML())).message_id;
+//   const messageId = ctx.callbackQuery.message.message_id;
+//   const message = await findSucceededMessageById(messageId);
+//   const count = ctx.callbackQuery.data.split(':')[1];
 
-  if (!message) {
-    await ctx.editMessageText('Expired');
-    return;
-  }
+//   if (!message) {
+//     await ctx.editMessageText('Expired');
+//     return;
+//   }
 
-  try {
-    await scrobbleTracks(multipleArray(message.tracks, count), undefined, ctx.user.key);
-  } catch (e) {
-    await requestError(ctx, e);
-    return;
-  }
+//   try {
+//     await scrobbleTracks(multipleArray(message.tracks, count), undefined, ctx.user.key);
+//   } catch (e) {
+//     await requestError(ctx, e);
+//     return;
+//   }
 
-  await successfulScrobble(ctx, undefined, message.tracks);
-});
+//   await successfulScrobble(ctx, undefined, message.tracks);
+// });
 
-bot.catch((e) => {
-  console.log(e);
-  sendToAdmin(`Unhandled Bot Error! ${e.message}`);
-});
+// bot.catch((e) => {
+//   console.log(e);
+//   sendToAdmin(`Unhandled Bot Error! ${e.message}`);
+// });
 
 module.exports = bot;
